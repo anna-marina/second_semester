65f14a99416f68452a58b2b9a43b84dcab535ea5
anna-marina <maryina.anna.va@gmail.com>
2015-04-05 00:52:52 +0300

    Update 3536.fs
    correct 35 36 and add 37 38

--- 3536.fs
+++ 3536.fs
@@ -1,4 +1,5 @@
 open System
+open System.IO
 open System.Text.RegularExpressions
 open System.Collections
 open System.Linq
@@ -56,12 +57,12 @@ type Parser (a : string []) =
                 ignore (it.MoveNext())
                 Plus (p.ParseElement())
 
+        | x when Char.IsDigit x.[0] ->
+                let r = Number (Int32.Parse x)
+                ignore (it.MoveNext())
+                r
         | x ->
-                let r =
-                    if Char.IsDigit x.[0] then
-                        Number (Int32.Parse x)
-                    else
-                        Id x
+                let r = Id x
                 ignore (it.MoveNext())
                 r
 
@@ -93,6 +94,7 @@ type Parser (a : string []) =
         ignore (it.MoveNext())
         p.ParseOperator 0
 
+
 let evalContext e (m : Map<string, int>) =
     let rec eval e =
         match e with
@@ -108,59 +110,136 @@ let evalContext e (m : Map<string, int>) =
         | Plus x -> eval x
     eval e
 
+let eval e =
+    evalContext e Map.empty
+
 let calcvar s m =
     evalContext (Parser(split s).Parse) (Map.ofList m)
 
 let calc s =
     calcvar s []
 
-[<TestFixture>]
-type TestCalc() =
-    
-    [<Test>]
-    member t.``Numbers`` () =
-        calc "0" |> should equal 0
-        calc "1" |> should equal 1
-        calc "12345" |> should equal 12345
+let toPostfix s =
+    let e = Parser(split s).Parse
+    let rec postfix e =
+        match e with
+        | Number x -> [x.ToString()]
+        | Id x -> [x]
+        | Add (a, b) -> (postfix a) @ (postfix b) @ ["+"]
+        | Sub (a, b) -> (postfix a) @ (postfix b) @ ["-"]
+        | Mul (a, b) -> (postfix a) @ (postfix b) @ ["*"]
+        | Div (a, b) -> (postfix a) @ (postfix b) @ ["/"]
+        | Rem (a, b) -> (postfix a) @ (postfix b) @ ["%"]
+        | Pow (a, b) -> (postfix a) @ (postfix b) @ ["^"]
+        (* Унарные операции с пометкой *)
+        | Minus x -> (postfix x) @ ["-!"]
+        | Plus x -> (postfix x) @ ["+!"]
+    postfix e
 
-    [<Test>]
-    member t.``Addition and subtraction`` () =
-        calc "2+3" |> should equal 5
-        calc "4-2" |> should equal 2
-        calc "1000-10" |> should equal 990
-        calc "0-1" |> should equal -1
-        calc "(2+(2+2))+(2+2)" |> should equal 10
+let fileToPostfix src dst =
+    let expr = String.Join("", File.ReadAllLines(src))
+    let post = String.Join(Environment.NewLine, toPostfix expr)
+    File.WriteAllText(dst, post)
 
-    [<Test>]
-    member t.``Unary`` () =
-        calc "-3" |> should equal -3
-        calc "--3" |> should equal 3
-        calc "2+-3" |> should equal -1
-        calc "-5++5" |> should equal 0
+let fromPostfix (e : string list) =
+    let rec translate e st =
+        match e, st with
+        | [], st -> st
+        | "+" :: e, b :: a :: st -> translate e ((Add (a, b)) :: st)
+        | "-" :: e, b :: a :: st -> translate e ((Sub (a, b)) :: st)
+        | "*" :: e, b :: a :: st -> translate e ((Mul (a, b)) :: st)
+        | "/" :: e, b :: a :: st -> translate e ((Div (a, b)) :: st)
+        | "%" :: e, b :: a :: st -> translate e ((Rem (a, b)) :: st)
+        | "^" :: e, b :: a :: st -> translate e ((Pow (a, b)) :: st)
+        | "+!" :: e, a :: st -> translate e ((Plus a) :: st)
+        | "-!" :: e, a :: st -> translate e ((Minus a) :: st)
+        | x :: e, st when Char.IsDigit x.[0] -> 
+            translate e (Number (Int32.Parse x) :: st)
+        | x :: e, st -> translate e ((Id x) :: st)
+    (translate e []).[0]
 
-    [<Test>]
-    member t.``Multiplication and division`` () =
-        calc "10-2*3" |> should equal 4
-        calc "6/2" |> should equal 3
-        calc "5/2*2" |> should equal 4
-        calc "12%5" |> should equal 2
+let fileToResult src dst =
+    let expr = List.ofArray (File.ReadAllLines(src))
+    let res = eval (fromPostfix expr)
+    File.WriteAllText(dst, res.ToString())
 
-    [<Test>]
-    member t.``Power`` () =
-        calc "2^2^3" |> should equal 256
-        calc "(2^2)^3" |> should equal 64
+let splitBy (s : string) c =
+    s.Split([|c|])
 
-    [<Test>]
-    member t.``Variables`` () =
-        calcvar "x" [("x", 6)] |> should equal 6
-        calcvar "-x" [("x", 6)] |> should equal -6
-        calcvar "x^2" [("x", 6)] |> should equal 36
-        calcvar "x*y" [("x", 6); ("y", 4)] |> should equal 24
-        calcvar "(x^y-v)/z" [("x", 5); ("y", 3); ("z", 2); ("v", 13)] 
-                |> should equal 56
+let parseMap (env : string) =
+    let pairArray l =
+        match l with
+        | [|x;y|] -> (x, Int32.Parse y)
+        | _ -> failwith "Error"
+    let v = splitBy (env.Replace(" ", "")) ','
+    let u = Array.map (fun s -> splitBy s '=') v
+    let mapping = Array.map pairArray u
+    List.ofArray mapping
+
+[<TestCase("0", Result=0)>]
+[<TestCase("1", Result=1)>]
+[<TestCase("12345", Result=12345)>]
+[<TestCase("2+3", Result= 5)>]
+[<TestCase("4-2", Result=2)>]
+[<TestCase("1000-10", Result=990)>]
+[<TestCase("0-1", Result=(-1))>]
+[<TestCase("(2+( 2 +2) ) +(2 + 2)", Result=10)>]
+[<TestCase("-3", Result=(-3))>]
+[<TestCase("-- 3", Result=3)>]
+[<TestCase("2+ -3", Result=(-1))>]
+[<TestCase("-5++  5", Result=0)>]
+[<TestCase("10-2*3", Result=4)>]
+[<TestCase("6 / 2", Result=3)>]
+[<TestCase("5/ 2*2", Result=4)>]
+[<TestCase("  12  % 5  ", Result=2)>]
+[<TestCase("2^2^3", Result=256)>]
+[<TestCase("(2 ^ 2) ^3", Result=64)>]
+[<TestCase(" -2 ^ 2", Result=4)>]
+[<TestCase("- (2 ^2)", Result=(-4))>]
+let TestCalc s =
+    calc s
+        
+[<TestCase("x+y", "x=6,y=5", Result=11)>]
+[<TestCase("x", "x=6", Result=6)>]
+[<TestCase("-x", "x=6", Result=(-6))>]
+[<TestCase("x^2", "x=6", Result=36)>]
+[<TestCase("x*y", "x=6,y=4", Result=24)>]
+[<TestCase("(x ^ y - v) / z", "x=5,y=3,z=2,v=13", Result=56)>]
+let TestCalcVar s (env : string) =
+    calcvar s (parseMap env)
+
+[<TestCase("2+2", Result="2 2 +")>]
+[<TestCase("0", Result="0")>]
+[<TestCase("-1", Result="1 -!")>]
+[<TestCase("5---5", Result="5 5 -! -! -")>]
+[<TestCase("1 + (2 / 3) ^2", Result="1 2 3 / 2 ^ +")>]
+[<TestCase("2 *2 *3", Result="2 2 * 3 *")>]
+[<TestCase("2 ^2 ^3", Result="2 2 3 ^ ^")>]
+let TestToPostfix s =
+    String.Join(" ", toPostfix s)
+[<TestCase("2 2 +", Result=4)>]
+[<TestCase("0", Result=0)>]
+[<TestCase("1 -!", Result=(-1))>]
+[<TestCase("5 5 -! -! -", Result=0)>]
+[<TestCase("1 2 3 / 2 ^ +", Result=1)>]
+[<TestCase("2 2 * 3 *", Result=12)>]
+[<TestCase("2 2 3 ^ ^", Result=256)>]
+[<TestCase("12 3 /", Result=4)>]
+let TestEvalPostfix (s : string) =
+    let v = splitBy s ' ' |> List.ofArray |> fromPostfix
+    evalContext v Map.empty
 
 [<EntryPoint>]
 let main argv =
     printfn "%A" (calc "1 + (2 / 3) ^2")
     printfn "%A" (calcvar "-x*3" [("x",5)])
+    let e = "5--5"
+    let p = toPostfix e
+    let ex = fromPostfix p
+    printfn "%A" p
+    printfn "%A" (calc e)
+    printfn "%A" (eval ex)
+    printfn "%A" (fromPostfix p |> eval)
+    fileToPostfix "expr.txt" "vertical.txt"
+    fileToResult "vertical.txt" "result.txt"
     0
