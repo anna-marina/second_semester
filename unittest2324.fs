943974945c2f32295c619c47a5fb64e8e8b0a497
anna-marina <maryina.anna.va@gmail.com>
2015-04-05 00:27:58 +0300

    Update unittest2324.fs
    TestCase

--- unittest2324.fs
+++ unittest2324.fs
@@ -1,4 +1,6 @@
+open System
 open NUnit.Framework
+
 (*20*)
 type IGraph =
     abstract AddEdge : int -> int -> unit
@@ -6,6 +8,9 @@ type IGraph =
     abstract HasEdge : int -> int -> bool
     abstract OutByEdge : int -> int list
     abstract Size : int
+
+let range k =
+    [0..k]
 
 (*21*)
 type MatrixGraph(n : int) =
@@ -22,7 +27,7 @@ type MatrixGraph(n : int) =
         member g.OutByEdge v =
             List.fold (fun s i -> 
                         if matrix.[v, i] then i :: s else s)
-                        [] [0 .. size-1]
+                        [] (range (size - 1))
         member g.Size =
             size
 
@@ -43,8 +48,8 @@ type ListGraph(n : int) =
         member g.Size =
             size
 
-let isVertice v x = 
-    v=x
+let isVertice v x =
+    v = x
 
 (*23*)
 let fromVertice (g : IGraph) v =
@@ -65,7 +70,32 @@ let toVertice (g : IGraph) v =
         List.exists (isVertice v) (fromVertice g x)
     let accumulateVertice acc x =
         if destReachableFrom x then x :: acc else acc
-    List.fold accumulateVertice [] [0 .. g.Size-1]
+    List.fold accumulateVertice [] (range (g.Size - 1))
+
+[<TestFixture>]
+type TestGraph () =
+    let mutable g = ListGraph (10) :> IGraph
+    let edges = [(0, 1); (2, 3); (1, 2);
+                 (4, 5); (6, 7); (7, 8);
+                 (8, 6)]
+    do
+        List.iter (fun (u, v) -> g.AddEdge u v) edges
+
+    [<TestCase(9,Result=[|9|])>]
+    [<TestCase(4,Result=[|4;5|])>]
+    [<TestCase(0,Result=[|0;1;2;3|])>]
+    [<TestCase(6,Result=[|6;7;8|])>]
+    member t.``From vertex`` v =
+        let f = fromVertice g v
+        Array.ofList (List.sort f)
+
+    [<TestCase(9,Result=[|9|])>]
+    [<TestCase(5,Result=[|4;5|])>]
+    [<TestCase(3,Result=[|0;1;2;3|])>]
+    [<TestCase(6,Result=[|6;7;8|])>]
+    member t.``To vertex`` v =
+        let f = toVertice g v
+        Array.ofList (List.sort f)
 
 (*25*)
 type IMarkedGraph<'T> =
@@ -92,99 +122,131 @@ type MarkedGraph<'T>(graph : IGraph) =
         member p.SetMark v m =
             mx.[v] <- m
         member p.GetMark v =
-            mx.[v]   
+            mx.[v]
 
-let randomNetwork sz =  
-    let getName k =
-        match k with
-        | 0 -> "Windows"
-        | 1 -> "MacOS"
-        | 2 -> "Linux"
-        | _ -> ""
-    let g = (MarkedGraph<string*bool>(ListGraph(sz)) :> IMarkedGraph<string*bool>)
+type Network (g : IMarkedGraph<string * bool>) =
     let r = System.Random()
-    let k = r.Next(g.Size * (g.Size - 1) / 2)
-    List.iter (fun i -> g.AddEdge i (i + 1); g.AddEdge (i + 1) i) [0 .. sz - 2]
-    let e = List.init k (fun i -> (r.Next(sz), r.Next(sz)))
-    List.iter (fun (s,t) -> if not (g.HasEdge s t) then g.AddEdge s t; g.AddEdge t s) e
-    let m = List.init sz (fun i -> (i, getName (r.Next(3))))
-    List.iter (fun (v,m) -> g.SetMark v (m, false)) m
-    g
 
-let simulateNet (g : IMarkedGraph<string * bool>) =
-    let r = System.Random()
-    let prob s =
-        match s with
+    let probability os =
+        match os with
         | "Windows" -> 0.9
         | "MacOS" -> 0.4
         | "Linux" -> 0.01
-        | _ -> 0.0
-    let markName m =
-        match m with
-        | true -> "Infected"
-        | false -> "OK"
-    let printNet () =
-        List.iter (fun i -> let (os, mark) = (g.GetMark i); 
-                            printfn "Computer %A with %A is %A" i os (markName mark)) [0 .. g.Size-1]
-    let someOfInfected l =
-        List.fold (fun s i -> (snd (g.GetMark i)) || s) false l
-    let allInfected () =
-        List.fold (fun s i -> (snd (g.GetMark i)) && s) true [0 .. g.Size-1]
-    let infect v =
-        let n = g.GetMark v
-        g.SetMark v (fst n, true)
-    let checkVertice v =
-        let x = (prob (fst ( g.GetMark v)))
-        let d = r.NextDouble()
-        if (someOfInfected (g.OutByEdge v)) && (d < x) then
-            infect v
-    let rec loop x =
-        if allInfected() then ()
+        | _ -> 1.0
+
+    member s.All =
+        (range (g.Size - 1))
+    
+    member s.NameOf v =
+        fst (g.GetMark v)
+
+    member s.IsInfected v =
+        snd (g.GetMark v)
+    
+    member s.Infect v =
+        g.SetMark v (s.NameOf v, true)
+
+    member s.ProbabilisticInfect v p =
+        if r.NextDouble() < p then s.Infect v
+
+    member s.InfectByOS v =
+        s.ProbabilisticInfect v (s.NameOf v |> probability)
+
+    member s.OneOfIsInfected l =
+        List.fold (fun a i -> s.IsInfected i || a) false l
+
+    member s.AllOfAreInfected l =
+        List.fold (fun a i -> s.IsInfected i && a) true l
+
+    member s.NeighbourIsInfected v =
+        s.OneOfIsInfected (g.OutByEdge v)
+
+    member s.AllAreInfected =
+        s.AllOfAreInfected s.All
+
+    member s.MayInfect v =
+        (not (s.IsInfected v)) && (s.NeighbourIsInfected v)
+
+    member s.DoStep () =
+        let mayInfect = List.filter s.MayInfect s.All
+        List.iter s.InfectByOS mayInfect
+    
+    member s.Render =
+        let side = g.Size |> float |> Math.Sqrt |> Math.Ceiling |> int
+        let concatStrings =
+            List.reduce (+)
+        let addNewline s =
+            s + "\n"
+        let mapTail m l =
+            match l with
+            | x :: xs -> x :: (List.map m xs)
+            | _ -> l
+        let evenLine i =
+            let lineList = List.init side (fun j -> if g.HasEdge (i * side + j) ((i - 1) * side + j) then "." else " ")
+            concatStrings (mapTail (fun i -> "   " + i) lineList)
+        let renderVertex i =
+            let name, v = g.GetMark i
+            let sub = if i % side > 0 then (if g.HasEdge i (i - 1) then ".." else "  ") else ""
+            sub + name.Substring(0, 1) + (if v then "-" else "+")
+        let lineIndex i =
+            [i * side..(i+1) * side - 1]
+        let renderLine i =
+            let lineList = List.map renderVertex (lineIndex i)
+            concatStrings lineList
+        let renderWithEven i =
+            if i > 0 then
+                (evenLine i) + "\n" + (renderLine i)
+            else
+                renderLine i
+        if side * side <> g.Size then
+            ""
         else
-            printfn "Step %i" (x + 1)
-            List.iter checkVertice [0 .. g.Size-1]
-            if x % 10 = 0 then
-                printNet()
-            loop (x + 1)
-    infect 0
-    loop 0
-    printNet()
+            let table = List.init side renderWithEven
+            concatStrings (List.map addNewline table)
 
+let generateSquareNetwork (size : int) =
+    let r = System.Random()
+    let g = MarkedGraph(ListGraph(size * size)) :> IMarkedGraph<string * bool>
+    let names = ["Windows"; "MacOS"; "Linux"]
+    let randomName () =
+        names.[r.Next(0, names.Length)]
+    let randomBool () =
+        r.Next(0, 100) < 60
+    let graphMarks = List.init (size * size) (fun i -> (randomName(), false))
+    let allVertices = (range (size * size - 1))
+    let allFrom x =
+        List.map (fun i -> (x, i)) [x + 1..size*size-1]
+    let allEdges = List.concat (List.map allFrom allVertices)
+    let correctEdge (u, v) =
+        (u + 1    = v && u / size = v / size) ||
+        (u + size = v && u % size = v % size)
+    let test = correctEdge (15,20)
+    let correctEdges = List.filter correctEdge allEdges
+    let selectedEdges = List.filter (fun i -> randomBool ()) correctEdges
+    List.iter (fun i -> g.SetMark i graphMarks.[i]) allVertices
+    List.iter (fun (u, v) -> g.AddEdge u v; g.AddEdge v u) selectedEdges
+    let net = Network g
+    net.Infect 0
+    net
 
-[<TestFixture>]
-type TestGraph () =
-    let mutable g = ListGraph (10) :> IGraph
-    do
-        g.AddEdge 0 1
-        g.AddEdge 2 3
-        g.AddEdge 1 2
-        g.AddEdge 4 5
-        g.AddEdge 6 7
-        g.AddEdge 7 8
-        g.AddEdge 8 6
+let simulateNet size steps =
+    let net = generateSquareNetwork size
+    for i in range steps do
+        net.DoStep()
+        if i % 10 = 0 then
+            let r = net.Render
+            printfn "State %d:\n%s" i r
 
-    [<Test>]
-    member t.``From vertex`` () =
-        let f = fromVertice g 9
-        Assert.AreEqual (List.sort f, [9])
-        let f = fromVertice g 4
-        Assert.AreEqual (List.sort f, [4;5])
-        let f = fromVertice g 0
-        Assert.AreEqual (List.sort f, [0;1;2;3])
-        let f = fromVertice g 6
-        Assert.AreEqual (List.sort f, [6;7;8])
-
-    [<Test>]
-    member t.``To vertex`` () =
-        let f = toVertice g 9
-        Assert.AreEqual (List.sort f, [9])
-        let f = toVertice g 5
-        Assert.AreEqual (List.sort f, [4;5])
-        let f = toVertice g 3
-        Assert.AreEqual (List.sort f, [0;1;2;3])
-        let f = toVertice g 6
-        Assert.AreEqual (List.sort f, [6;7;8])
-             
 [<EntryPoint>]
 let main argv = 
+    let g = MatrixGraph(10) :> IGraph
+    g.AddEdge 3 0
+    g.AddEdge 5 4
+    g.AddEdge 3 2
+    g.AddEdge 4 3
+    printfn "%A" (toVertice g 0)
+    printfn "%A" (fromVertice g 4)
+
+    simulateNet 5 100
+
     0
